cfc6bdc503b2a0accd47ad7621fd72878cc7d0ba
Paul Potsides <strongpauly@hotmail.com>
2017-07-20 19:03:54 +0100

    Score was being wiped.

--- src/state/reducers/score.js
+++ src/state/reducers/score.js
@@ -9,9 +9,9 @@ export default function(score=null, action) {
   if(action.type === 'START') {
     score = Object.assign({}, defaultScore);
   } else if (action.type === 'BLOCK_STOP') {
-    score = Object.assign({}, {numBlocks: score.numBlocks + 1});
+    score = Object.assign({}, score, {numBlocks: score.numBlocks + 1});
   } else if (action.type === 'REMOVE_LINES') {
-    score = Object.assign({}, {numLines: score.numLines + action.payload.length});
+    score = Object.assign({}, score, {numLines: score.numLines + action.payload.length});
   }
   return score;
 }
